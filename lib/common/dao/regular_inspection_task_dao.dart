0b8c97fc95b548ac20657624ad1abf7de2207f58
ggq18663278150@sina.com <5515225gg5>
2019-08-12 13:50:52 +0800

    fix: 更改扫码的路由方式，不使用框架，直接将变量传入

---
+++ lib/common/dao/regular_inspection_task_dao.dart
@@ -0,0 +1,34 @@
+/////@author ggq
+/////@description: 日常巡检任务的数据持久化层
+/////@date :2019/8/12 13:21
+part of "dao.dart";
+//part 'regular_inspection_task_dao.g.dart';
+//
+//@DataClassName("RegularInspectionTaskDetailEntry")
+//class RegularInspectionTaskDetailEntry extends Table {
+//  TextColumn get deviceId => text()();
+//
+//  TextColumn get deviceName => text()();
+//
+//  TextColumn get deviceType => text()();
+//
+//  TextColumn get inspectionRequire => text()();
+//
+//  TextColumn get inspectionResultType => text()();
+//
+//  TextColumn get processType => text()();
+//
+//  TextColumn get noteText => text()();
+//
+//  BlobColumn get images => blob()();
+//
+//  TextColumn get taskStatus => text()();
+//
+//  TextColumn get taskArea => text()();
+//
+//  TextColumn get taskFloor => text()();
+//}
+//
+//@UseMoor(tables: [RegularInspectionTaskDetailEntry])
+//class RITaskDetailDatabase {
+//}
