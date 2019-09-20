089f8398fc2ad0692ff126f5c0dbdf70ea0651c4
ggq18663278150@sina.com <5515225gg5>
2019-09-20 14:38:24 +0800

    feat: 添加维保工作人员的页面

---
+++ lib/property_worker/property_worker.dart
@@ -0,0 +1,122 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ifcy/building_owner/blocs/building_owner_blocs.dart';
+import 'package:ifcy/building_owner/repositories/building_owner_repositories.dart';
+import 'package:ifcy/common/components/components.dart';
+import 'package:ifcy/device_supervisor/blocs/supervisor_blocs.dart';
+import 'package:ifcy/device_supervisor/repositories/repositories.dart';
+import 'package:ifcy/main_app/blocs/main_app_blocs.dart';
+import 'package:ifcy/property_manager/pages/property_manager_pages.dart';
+
+class PropertyWorker extends StatefulWidget {
+  @override
+  _PropertyWorkerState createState() => _PropertyWorkerState();
+}
+
+class _PropertyWorkerState extends State<PropertyWorker> {
+  List<Widget> viewList = [];
+  int currentIndex = 0;
+  List<Icon> iconList = [
+    Icon(Icons.business),
+    Icon(Icons.person_outline),
+  ];
+  List<String> iconTextList = [
+    "大厦",
+    "我的",
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    viewList
+      ..add(MonitorPage(() => Scaffold.of(context).openDrawer()))
+      ..add(PersonPage(() => Scaffold.of(context).openDrawer()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<MonitorRepositories>(
+            builder: (context) => MonitorRepositories(),
+          ),
+        ],
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<MonitorBloc>(
+              builder: (context) => MonitorBloc(
+                  RepositoryProvider.of<MonitorRepositories>(context))
+                ..dispatch(FetchMonitorDataEvent()),
+            ),
+            BlocProvider<PlanListBloc>(
+              builder: (context) {
+                PlanListRepositories repositories = PlanListRepositories();
+                return PlanListBloc(repositories);
+              },
+            ),
+            BlocProvider<TaskListBloc>(
+              builder: (context) {
+                TaskListRepositories repositories = TaskListRepositories();
+                return TaskListBloc(repositories);
+              },
+            ),
+            BlocProvider<DeclareMessageBloc>(
+              builder: (context) {
+                return DeclareMessageBloc(ReportDeviceRepositories())
+                  ..dispatch(FetchDeclareMessage());
+              },
+            ),
+            BlocProvider<EmployeeListBloc>(
+              builder: (context) {
+                return EmployeeListBloc(EmployeeRepositories())
+                  ..dispatch(FetchEmployeeListDataEvent());
+              },
+            ),
+            BlocProvider<DeviceMessageBloc>(
+              builder: (context) {
+                DeviceMessageRepositories repo = DeviceMessageRepositories();
+                return DeviceMessageBloc(repo)..dispatch(FetchAllDevices());
+              },
+            ),
+            BlocProvider<BadgeBloc>(
+              builder: (context) => BadgeBloc([1, 0],
+                  monitorBloc: BlocProvider.of<MonitorBloc>(context)),
+            ),
+          ],
+          child: Scaffold(
+            bottomNavigationBar:
+                BlocBuilder<BadgeBloc, List<int>>(builder: (context, state) {
+              return BottomNavigationBar(
+                elevation: 10,
+                type: BottomNavigationBarType.fixed,
+                items: iconList.map<BottomNavigationBarItem>((i) {
+                  int currentIndex = iconList.indexOf(i);
+                  int currentBadge = state[currentIndex];
+                  return BottomNavigationBarItem(
+                    icon: Badge(
+                      child: i,
+                      badgeContent: Text(
+                        currentBadge.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      showBadge: currentBadge != 0,
+                    ),
+                    title: Text(
+                      iconTextList[currentIndex],
+                    ),
+                  );
+                }).toList(),
+                onTap: (v) {
+                  setState(() {
+                    currentIndex = v;
+                  });
+                },
+                currentIndex: currentIndex,
+              );
+            }),
+            body: viewList[currentIndex],
+          ),
+        ));
+  }
+}
